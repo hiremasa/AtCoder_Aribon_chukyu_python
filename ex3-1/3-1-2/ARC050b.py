fe110929f122d53600de441c5b457bbd1849237f
hiremasa <syorikunn0901@gmail.com>
2020-09-25 08:06:25 +0900

    binary_search

--- ex3-1/3-1-2/ARC050b.py
+++ ex3-1/3-1-2/ARC050b.py
@@ -1,6 +1,3 @@
-R, B = map(int, input().split())
-x, y = map(int, input().split())
-
 def check(K):
 	cnt=(R-K)//(x-1) + (B-K)//(y-1)
 	return cnt>=K and R >= K and B >= K
@@ -17,5 +14,26 @@ def binary_search():
 	return l
 
 if __name__=="__main__":
+	R, B = map(int, input().split())
+	x, y = map(int, input().split())
 	print(int(binary_search()))
 
+############### meguru bisect ###############
+R, B = map(int,input().split())
+x, y = map(int,input().split())
+def is_ok(K):
+	if K>R or K>B:
+		return False
+	if (R-K)//(x-1) + (B-K)//(y-1) >= K:
+		return True
+	return False
+
+def meguru_bisect(ng, ok):
+	while (abs(ok - ng) > 1):
+		mid = (ok + ng) // 2
+		if is_ok(mid):
+			ok = mid
+		else:
+			ng = mid
+	return ok
+print(meguru_bisect(10**19,0))
